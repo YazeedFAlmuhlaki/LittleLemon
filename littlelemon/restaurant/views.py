0b3a513e47c0e487e499c76bb9d21257e317116b
YazeedFAlmuhlaki <yazeedalmuhlaki.pa@icloud.com>
2025-08-03 05:42:50 +0300

    creating the api.

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateAPIView,DestroyAPIView
+from .serializers import MenuSerializer
+from .models import Menu
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
